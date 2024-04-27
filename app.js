25f3f9b9781f1a38679fce8856053c7df1c29005
Maks Samborski <makxc7@gmail.com>
2024-04-27 18:51:59 +0200

    .

--- app.js
+++ app.js
@@ -2,5 +2,106 @@ let roughSvg = rough.svg(document.getElementById("svg"));
 
 const svg = document.getElementById("svg");
 
-var globalY = 50;
-var globalX = 300;
+var globalY = 300;
+var globalX = 875;
+
+const test = roughSvg.rectangle(globalX, globalY, 100, 100, {
+  fill: "rgb(255, 0, 200, 0.2)",
+  fillStyle: "solid",
+});
+svg.appendChild(test);
+
+const up = document.querySelector("#up");
+up.addEventListener("click", () => {
+  console.log("add");
+  upBlock(100, globalY);
+});
+
+const down = document.querySelector("#down");
+down.addEventListener("click", () => {
+  console.log("add");
+  downBlock(100, globalY);
+});
+const left = document.querySelector("#left");
+left.addEventListener("click", () => {
+  console.log("add");
+  leftBlock(100, globalY);
+});
+
+const right = document.querySelector("#right");
+right.addEventListener("click", () => {
+  console.log("add");
+  rightBlock(100, globalY);
+});
+
+function upBlock(x, y) {
+  const test = roughSvg.rectangle(globalX, globalY - 130, 100, 100, {
+    fill: "rgb(255, 0, 200, 0.2)",
+    fillStyle: "solid",
+  });
+  test.setAttribute("id", "blok");
+  test.addEventListener("click", () => {
+    console.log("click");
+  });
+  svg.appendChild(test);
+  const line = roughSvg.line(
+    globalX + 50,
+    globalY + 100 - 130,
+    globalX + 50,
+    globalY + 100 + 30 - 130
+  );
+  svg.appendChild(line);
+  globalY = globalY - 130;
+}
+
+function downBlock(x, y) {
+  const test = roughSvg.rectangle(globalX, globalY + 130, 100, 100, {
+    fill: "rgb(255, 0, 200, 0.2)",
+    fillStyle: "solid",
+  });
+  test.addEventListener("click", () => {
+    console.log("click");
+  });
+  svg.appendChild(test);
+  const line = roughSvg.line(
+    globalX + 50,
+    globalY + 100,
+    globalX + 50,
+    globalY + 100 + 30
+  );
+  svg.appendChild(line);
+  globalY = globalY + 130;
+}
+
+function leftBlock(x, y) {
+  const test = roughSvg.rectangle(globalX - 130, globalY, 100, 100, {
+    fill: "rgb(255, 0, 200, 0.2)",
+    fillStyle: "solid",
+  });
+  test.addEventListener("click", () => {
+    console.log("click");
+  });
+  svg.appendChild(test);
+  const line = roughSvg.line(globalX - 30, globalY + 50, globalX, globalY + 50);
+  svg.appendChild(line);
+  globalX = globalX - 130;
+}
+
+function rightBlock(x, y) {
+  const test = roughSvg.rectangle(globalX + 130, globalY, 100, 100, {
+    fill: "rgb(255, 0, 200, 0.2)",
+    fillStyle: "solid",
+  });
+  test.addEventListener("click", () => {
+    console.log("click");
+  });
+  svg.appendChild(test);
+  const line = roughSvg.line(
+    globalX + 100,
+    globalY + 50,
+    globalX + 100 + 30,
+    globalY + 50
+  );
+  svg.appendChild(line);
+  globalX = globalX + 130;
+}
